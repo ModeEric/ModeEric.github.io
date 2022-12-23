e85b436bf3070054b74b21eafa3fe608e29763eb
CommonEchidna <ericjm4@illinois.edu>
2022-12-23 10:11:49 -0500

    testing drawing

---
+++ css&js/testpage.js
@@ -0,0 +1,40 @@
+function tohex(num){
+    var hex = num.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+function drawhelix() {
+var pointSize = 1; // Change according to the size of the point.
+var canvas = document.getElementById("canvas");
+
+canvas.width  = window.innerWidth;
+canvas.height = window.innerHeight;
+var ctx = document.getElementById("canvas").getContext("2d");
+
+ctx.fillStyle = "#373737"; 
+var x=0;
+var y=0;
+
+randomstart = (Math.random())*Math.PI*2;
+
+for(var j = 0; j < 2; j++) {
+    if(j==1){
+        randomstart=randomstart+5*Math.PI/6;
+    }
+
+    var currnum=0;
+
+for(var i = 0; i < 25000; i++) {
+
+
+ctx.beginPath(); //Start path
+x = 200*Math.sin(i/1000-50+randomstart)+window.innerWidth/2+(Math.random()-Math.random())*Math.sqrt(i/4);
+y = i/20;
+
+ctx.arc(x,y, pointSize, 0, Math.PI * 2, true); // Draw a point using the arc function of the canvas with a point structure.
+ctx.fill();
+}
+}
+console.log("DONE");
+}
+
+window.addEventListener("DOMContentLoaded", drawhelix);
