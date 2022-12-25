0db587abf0554dfa2c1c12e24cb18c289f804d92
CommonEchidna <ericjm4@illinois.edu>
2022-12-25 14:41:35 -0800

    blog and testpage

--- css&js/testpage.js
+++ css&js/testpage.js
@@ -10,16 +10,14 @@ canvas.width  = window.innerWidth;
 canvas.height = window.innerHeight;
 var ctx = document.getElementById("canvas").getContext("2d");
 
-ctx.fillStyle = "#373737"; 
+ctx.fillStyle = "#092803"; 
 var x=0;
 var y=0;
 
 randomstart = (Math.random())*Math.PI*2;
 
-for(var j = 0; j < 2; j++) {
-    if(j==1){
-        randomstart=randomstart+5*Math.PI/6;
-    }
+for(var j = 0; j < 10; j++) {
+    randomstart=randomstart+1*Math.PI/5;
 
     var currnum=0;
 
@@ -27,8 +25,8 @@ for(var i = 0; i < 25000; i++) {
 
 
 ctx.beginPath(); //Start path
-x = 200*Math.sin(i/1000-50+randomstart)+window.innerWidth/2+(Math.random()-Math.random())*Math.sqrt(i/4);
-y = i/20;
+x = 200*Math.sin(i/1000-50+randomstart)+window.innerWidth/2+Math.sqrt((Math.random()-Math.random())*i/4);
+y = i/20; 
 
 ctx.arc(x,y, pointSize, 0, Math.PI * 2, true); // Draw a point using the arc function of the canvas with a point structure.
 ctx.fill();
